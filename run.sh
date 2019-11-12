b02a1a9e6e432680498e8cbe97e65b70cc544e5f
vanloc <loconeightwo@gmail.com>
2019-11-12 18:58:25 +0700

    mylinux

--- run.sh
+++ run.sh
@@ -7,10 +7,8 @@ fc-cache -f -v
 # sudo apt install gconf-editor
 # gconftool-2 --set /apps/gnome-terminal/profiles/Default/font --type string "CascadiaCode Nerd Font Regular 12"
 # sudo apt purge gconf-editor
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 sudo bash $DIRECTORY/Grub/install-vimix.sh
-sudo cp $DIRECTORY/lockscreen.jpg $DIRECTORY/wallpaper.jpg /usr/share/backgrounds
+sudo cp $DIRECTORY/image/lockscreen.jpg $DIRECTORY/image/wallpaper.jpg /usr/share/backgrounds
 gsettings set org.gnome.desktop.background picture-uri file:///usr/share/backgrounds/wallpaper.jpg
 gsettings set org.gnome.desktop.screensaver picture-uri file:///usr/share/backgrounds/lockscreen.jpg
 # sudo perl -i -0pe 's/#lockDialogGroup \{\n   background: #2c001e url\(noise-texture.png\);\n   background-repeat: repeat; \n\}/#lockDialogGroup \{\n   background: #2c001e url\(file:\/\/\/usr\/share\/backgrounds\/lockscreen.jpg\);\n   background-repeat: no-repeat;\n   background-size: cover;\n   background-position: center;\n\}/i' /usr/share/gnome-shell/theme/gdm3.css
@@ -19,3 +17,5 @@ cp $DIRECTORY/.zshrc ~/
 cp -r $DIRECTORY/vscode/.vscode ~/
 cp -r $DIRECTORY/vscode/Code ~/.config/
 timedatectl set-local-rtc 1 --adjust-system-clock
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
