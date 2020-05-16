4720794065a34714bf3e26fe9170abead1d00dcf
vanloc <vanloc18021998@gmail.com>
2020-05-16 19:07:30 +0700

    update for popos

--- .zshrc
+++ .zshrc
@@ -1,18 +1,25 @@
 export ZSH="/home/vanloc/.oh-my-zsh"
+source $(dirname $(gem which colorls))/tab_complete.sh
 ZSH_THEME="powerlevel9k/powerlevel9k"
-plugins=(git adb pip)
+plugins=(git adb pip virtualenv)
 source $ZSH/oh-my-zsh.sh
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=22'
 POWERLEVEL9K_MODE="nerdfont-complete"
 POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(custom_icon dir vcs)
-POWERLEVEL9K_DISABLE_RPROMPT=true
+POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(virtualenv custom_quote)
+POWERLEVEL9K_DISABLE_RPROMPT=false
 POWERLEVEL9K_VCS_BRANCH_ICON=$'\uF126 '
 POWERLEVEL9K_CUSTOM_ICON="echo 﫵"
 POWERLEVEL9K_CUSTOM_ICON_FOREGROUND="red"
 POWERLEVEL9K_CUSTOM_ICON_BACKGROUND="black"
+POWERLEVEL9K_CUSTOM_QUOTE='echo "I can do it "'
 POWERLEVEL9K_HOME_ICON=''
 POWERLEVEL9K_HOME_SUB_ICON=''
 POWERLEVEL9K_FOLDER_ICON=''
+
+alias ls='colorls --group-directories-first'
+alias l='colorls --group-directories-first --almost-all'
+alias ll='colorls --group-directories-first --almost-all --long'
 
 alias cls='printf "\033c"'
 
@@ -28,3 +35,7 @@ function _pip_completion {
 compctl -K _pip_completion pip3
 # pip zsh completion end
 
+filepush() { if [ $# -eq 0 ]; then echo -e "No arguments specified. Usage:\necho filepush /tmp/test.md\ncat /tmp/test.md | filepush test.md"; return 1; fi
+tmpfile=$( mktemp -t filepushXXX ); if tty -s; then basefile=$(basename "$1" | sed -e 's/[^a-zA-Z0-9._-]/-/g'); curl --progress-bar --upload-file "$1" "https://filepush.co/upload/$basefile" >> $tmpfile; else curl --progress-bar --upload-file "-" "https://filepush.co/upload/$1" >> $tmpfile ; fi; cat $tmpfile; rm -f $tmpfile; }
+
+# printf "\033c"
--- run.sh
+++ run.sh
@@ -1,21 +1,14 @@
 sudo apt install git
-sudo apt install python3-pip gnome-tweak-tool gnome-shell-extensions curl zsh
+sudo apt install python3-pip gnome-tweak-tool curl zsh ruby ruby-dev
+sudo gem install colorls
 chsh -s $(which zsh)
 DIRECTORY=$(cd `dirname $0` && pwd)
 sudo cp $DIRECTORY/fonts/* /usr/share/fonts/truetype/
 fc-cache -f -v
-# sudo apt install gconf-editor
-# gconftool-2 --set /apps/gnome-terminal/profiles/Default/font --type string "CascadiaCode Nerd Font Regular 12"
-# sudo apt purge gconf-editor
-sudo bash $DIRECTORY/Grub/install-vimix.sh
-sudo cp $DIRECTORY/image/lockscreen.jpg $DIRECTORY/image/wallpaper.jpg /usr/share/backgrounds
-gsettings set org.gnome.desktop.background picture-uri file:///usr/share/backgrounds/wallpaper.jpg
-gsettings set org.gnome.desktop.screensaver picture-uri file:///usr/share/backgrounds/lockscreen.jpg
-# sudo perl -i -0pe 's/#lockDialogGroup \{\n   background: #2c001e url\(noise-texture.png\);\n   background-repeat: repeat; \n\}/#lockDialogGroup \{\n   background: #2c001e url\(file:\/\/\/usr\/share\/backgrounds\/lockscreen.jpg\);\n   background-repeat: no-repeat;\n   background-size: cover;\n   background-position: center;\n\}/i' /usr/share/gnome-shell/theme/gdm3.css
-sudo bash $DIRECTORY/Plymouth/install.sh
-cp $DIRECTORY/.zshrc ~/
-cp -r $DIRECTORY/vscode/.vscode ~/
 cp -r $DIRECTORY/vscode/Code ~/.config/
 timedatectl set-local-rtc 1 --adjust-system-clock
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+cp $DIRECTORY/.zshrc ~/
+chmod +x $DIRECTORY/wallpaper.sh
+sudo cp $DIRECTORY/wallpaper.sh /usr/bin/
